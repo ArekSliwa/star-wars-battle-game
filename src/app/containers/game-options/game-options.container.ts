23198cb582552072282074899aaaaf8dcc8c7cb5
Arkadiusz Śliwa <arek.sliwa@gmail.com>
2019-12-06 18:13:47 +0100

    add space stars background

--- src/app/containers/game-options/game-options.container.ts
+++ src/app/containers/game-options/game-options.container.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {BattleUnitSwitcherDialogContainerComponent} from '..';
 import * as fromGame from 'store/index';
 import {Store} from '@ngrx/store';
